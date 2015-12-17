9a865eba866e65ff76a45ef4d66822a1c3b1db28
Nitin <nitin.upadhyay@vodafone.com>
2015-12-17 20:41:44 +0530

    DB scripts.

--- learning/DBScript.sql
+++ learning/DBScript.sql
@@ -9,3 +9,23 @@ CREATE TABLE `Student` (
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 commit;
 
+create database stockdb;
+
+use stockdb;
+ 
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`category_id`)
+);
+ 
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(512) NOT NULL,
+  `price` float NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`),
+  KEY `fk_category` (`category_id`),
+  CONSTRAINT `fk_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`)
+);
